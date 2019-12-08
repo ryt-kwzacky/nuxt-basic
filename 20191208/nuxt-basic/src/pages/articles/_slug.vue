3e2ca1c4650930cfe361614d1b5b80b20a0e6bf8
RyutoKawasaki <various.use.0416@gmail.com>
2019-12-08 21:30:49 +0900

    microCMS test

--- 20191208/nuxt-basic/src/pages/articles/_slug.vue
+++ 20191208/nuxt-basic/src/pages/articles/_slug.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
     <h1>_slug: {{ slug }}</h1>
+    <p>{{ apiItems }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
       slug: this.$route.params.slug
+    }
+  },
+
+  async asyncData() {
+    const { data } = await axios.get(process.env.microCmsUrl + '/test', {
+      headers: { 'X-API-KEY': process.env.API_KEY }
+    })
+    return {
+      apiItems: data.contents
     }
   }
 }
